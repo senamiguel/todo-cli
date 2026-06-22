fdb815529a698451315bb30f4e6e517fc9d0f9d7
Miguel Sena <miguelaugustosena@gmail.com>
2026-06-22 13:31:16 -0300

    refactor: implement auto-incrementing task IDs and update ID type to usize across the store and CLI

--- src/task_store.rs
+++ src/task_store.rs
@@ -2,20 +2,24 @@ use std::collections::{HashMap};
 use crate::task::{Task};
 
 pub struct TaskStore{
-    tasks: HashMap<u8,Task>
+    tasks: HashMap<usize,Task>,
+    next_id: usize
 }
 impl TaskStore {
     pub fn new() -> Self{
-        Self { tasks: HashMap::new() }
+        Self { 
+            tasks: HashMap::new(),
+            next_id : 1
+         }
     }
-    pub fn remove(&mut self, id:u8)-> bool{
+    pub fn remove(&mut self, id:usize)-> bool{
         if !self.check_task_exists(id) {
             return false;
         }
         self.tasks.remove(&id);
         true
     }
-    pub fn toggle_status(&mut self, id: u8) -> bool{
+    pub fn toggle_status(&mut self, id: usize) -> bool{
         if let Some(task) = self.tasks.get_mut(&id) {
             task.toggle();
             return true;
@@ -28,21 +32,19 @@ impl TaskStore {
         }
         tasks_list
     }
-    pub fn create_task(&mut self, id: u8, description: String)-> bool{
-        let task = Task::new(id, description);
-        if self.check_task_exists(id) {
-            return false;
-        }
+    pub fn create_task(&mut self,description: String)-> bool{
+        let task = Task::new(self.next_id, description);
+        self.next_id += 1;
         self.tasks.entry(task.id).or_insert(task);
         true
     }
-    pub fn set_description(&mut self, id: u8, desc: String)-> bool {
+    pub fn set_description(&mut self, id: usize, desc: String)-> bool {
         if let Some(task) = self.tasks.get_mut(&id){
             task.set_description(desc);
             return true;
         }false
     }
-    pub fn check_task_exists(&self, id: u8) -> bool {
+    pub fn check_task_exists(&self, id: usize) -> bool {
         self.tasks.contains_key(&id)
     }
 
